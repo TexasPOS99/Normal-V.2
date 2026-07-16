109569190f396d0dc5b1a32fed580f58601369f7
TexasPOS99 <normalzname111@gmail.com>
2026-07-16 11:33:01 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// วางค่าจาก Firebase Console > Project settings > Your apps > Web app
+// Firebase Web App configuration
 window.FIREBASE_CONFIG = {
-  apiKey: "ใส่ค่า-apiKey",
-  authDomain: "ใส่ค่า-project.firebaseapp.com",
-  projectId: "ใส่ค่า-projectId",
-  storageBucket: "ใส่ค่า-project.firebasestorage.app",
-  messagingSenderId: "ใส่ค่า-messagingSenderId",
-  appId: "ใส่ค่า-appId"
+  apiKey: "AIzaSyA44BkASak5_vuXXqJMnvsKaLUxLTAUUnI",
+  authDomain: "normal-v2.firebaseapp.com",
+  projectId: "normal-v2",
+  storageBucket: "normal-v2.firebasestorage.app",
+  messagingSenderId: "905137466324",
+  appId: "1:905137466324:web:55d002f5b32f7f84a3c441",
+  measurementId: "G-ZM12P90QT0"
 };
